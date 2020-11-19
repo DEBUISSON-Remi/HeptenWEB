456f52f33bd7fedb382c63a247d490b6f4f89465
rémi <debuis22re@lycee-ndduroc.com>
2020-11-19 17:23:46 +0100

    récupération des données de l'api pour la page devis

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,10 @@
 <!doctype html>
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/pages/devis.blade.php
+++ resources/views/pages/devis.blade.php
@@ -1,3 +1,114 @@
 @extends('layouts.app')
 
 @section('title','devis')
+
+@section('content')
+    <?php
+
+
+    $curl = curl_init('http://127.0.0.1:8001/api/devis');
+    curl_setopt_array($curl, [
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 2
+    ]);
+    $data = curl_exec($curl);
+    $dataTable = json_decode($data, true);
+    $devisAttente= 0;
+    $long = count($dataTable)-1;
+    for ($i = 0; $i <= $long; $i++){
+        if ($dataTable[$i]['etatDevis'] == "1") {
+            $devisAttente ++;
+        }
+    }
+
+    echo $devisAttente;
+
+    curl_close($curl);
+    echo " nombre de demande en attente d'être traitée pour un devis";
+
+
+
+    echo"<br/>";
+
+
+    $curl = curl_init('http://127.0.0.1:8001/api/devis');
+    curl_setopt_array($curl, [
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 2
+    ]);
+    $data = curl_exec($curl);
+    $dataTable = json_decode($data, true);
+    $devisAttente= 0;
+    $long = count($dataTable)-1;
+    for ($i = 0; $i <= $long; $i++){
+        if ($dataTable[$i]['valide'] == "") {
+            $devisAttente = $devisAttente + 1;
+        }
+    }
+
+    echo $devisAttente;
+
+    curl_close($curl);
+echo " le nombre de devis en attente d'être accepté par les clients";
+
+
+    echo "<br/>";
+
+    $curl = curl_init('http://127.0.0.1:8001/api/devis');
+    curl_setopt_array($curl, [
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 2
+    ]);
+    $data = curl_exec($curl);
+    $dataTable = json_decode($data, true);
+    $devisAttente= 0;
+    $long = count($dataTable)-1;
+    for ($i = 0; $i <= $long; $i++){
+        if ($dataTable[$i]['valide'] == "0") {
+            $devisAttente = $devisAttente + 1;
+        }
+    }
+
+    echo $devisAttente;
+
+    curl_close($curl);
+echo " le nombre de devis refusés ou abandonnés";
+
+
+    echo "<br/>";
+
+
+    $curl = curl_init('http://127.0.0.1:8001/api/devis');
+    curl_setopt_array($curl, [
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 2
+    ]);
+    $data = curl_exec($curl);
+    $dataTable = json_decode($data, true);
+    $countNonTraitee= 0;
+    $countTraite = 0;
+    $long = count($dataTable)-1;
+    for ($i = 0; $i <= $long; $i++){
+
+        if (strtotime( $dataTable[$i]['dateArriveePrevue'] )>=strtotime(date('d m Y'))){
+            if ($dataTable[$i]['valide'] == "1"){
+                $countTraite ++;
+            }
+        }else {
+            if ($dataTable[$i]['valide'] == "1"){
+                $countNonTraitee++;
+                }
+        }
+
+    }
+
+    echo $countNonTraitee . " le nombre de devis acceptés mais non-traités par le responsable logistique de son agence";
+    echo "<br/>";
+    echo $countTraite ." le nombre de devis acceptés et traités par le responsable logistique de son agence";
+
+    ?>
+@endsection
